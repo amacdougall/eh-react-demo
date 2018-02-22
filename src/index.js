ec058c3377a82786cf42bc64cddbdea9497f4e62
Alan MacDougall <smoke@alanmacdougall.com>
2018-02-22 12:07:15 -0500

    Integrate redux-devtools, react-redux, classnames.
    These will speed development of the UI.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
-import { reducer } from "./redux/state";
 
-let store = createStore(reducer);
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { reducer, initialState } from "./redux/state";
+
+// dev
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// debug
+import { getPatients } from "./redux/actionCreators";
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 window.store = store; // debug
+window.getPatients = getPatients;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
